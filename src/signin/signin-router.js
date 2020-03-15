c9313c05c81b01722e73c98a4cda234992b020cd
George Arnold <george.arnold.88@gmail.com>
2020-03-15 19:45:47 -0400

    fixing signing to limit signing

--- src/signin/signin-router.js
+++ src/signin/signin-router.js
@@ -2,39 +2,53 @@ const express = require('express');
 const signinRouter = express.Router();
 const bodyParser = express.json();
 const bcrypt = require('bcrypt-nodejs');
-const SigninService = require('./signin-service');
 
-const serializeRegister = user => ({
-  id: user.id,
-  email: user.email,
-  name: user.name,
-  joined: new Date()
-});
-
-const serializeSignin = user => ({
-  id: user.id,
-  email: user.email,
-
-})
-
-signinRouter.route('/signin').post(bodyParser, (req, res, next) => {
-
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
-});
-
-signinRouter.route('/register').post(bodyParser, (req, res, next) => {
-  const { email, name, password } = req.body;
-  const newUser = { email, name, joined: new Date() };
-
-  SigninService.insertUser(req.app.get('db'), newUser)
-    .then(user => {
-      res.status(201).json(serializeRegister(user));
+signinRouter.route('/signin').post(bodyParser, (req, res) => {
+  req.app
+    .get('db')
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return req.app
+          .get('db')
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('first wrong credentials');
+      }
     })
-    .catch(next);
+    .catch(err => res.status(400).json('second wrong credentials'));
+});
+signinRouter.route('/register').post(bodyParser, (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  req.app.get('db').transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({ email: loginEmail[0], name: name, joined: new Date() })
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  });
 });
 
 module.exports = signinRouter;
