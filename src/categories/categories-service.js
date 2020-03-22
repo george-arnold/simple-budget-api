557c0b073aef1cac6cc55d25e33f1bdacc0b5375
George Arnold <george.arnold.88@gmail.com>
2020-03-22 19:26:34 -0400

    commit

--- src/categories/categories-service.js
+++ src/categories/categories-service.js
@@ -1,28 +1,37 @@
 const CategoryService = {
   getAllCategories(knex, userId) {
-    return knex.select('*').from('categories').where('user_id','=', userId );
-  },         
+    return knex
+      .select('*')
+      .from('categories')
+      .where('user_id', '=', userId);
+  },
 
   insertCategory(knex, newCategory) {
     return knex
-    .insert(newCategory)
-    .into('categories')
-    .returning('*') //what's this
-    .then(rows => {
-      return rows[0]; // and this
-    });
+      .insert(newCategory)
+      .into('categories')
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
   },
-  getById(knex,categoryId){
-    return knex.from('categories').select('*').where('id', categoryId).first();
+  getById(knex, categoryId) {
+    return knex
+      .from('categories')
+      .select('*')
+      .where('id', categoryId)
+      .first();
   },
-  deleteCategory(knex,id) {
-    return knex('categories').where({id}).delete();
+  deleteCategory(knex, id) {
+    return knex('categories')
+      .where({ id })
+      .delete();
   },
   updateCategory(knex, id, newCategory) {
     return knex('categories')
       .where({ id })
       .update(newCategory);
   }
-}
+};
 
-module.exports = CategoryService
+module.exports = CategoryService;
